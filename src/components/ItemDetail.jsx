351fed3064b78e1abd1307690e09a73eaca14a19
Jean <jeanncavalcante@gmail.com>
2023-08-23 21:09:22 -0300

    eventos

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,10 +1,24 @@
+import ItemCount from "./ItemCount";
+import { useState } from "react";
+
 function ItemDetail({ item }) {
+  const [addedQuantity, setAddedQuantity] = useState(null);
+  const [count, setCount] = useState(1);
+
+  const handleAddToCart = (quantityToAdd) => {
+    setAddedQuantity(quantityToAdd);
+  };
+
   return (
     <div className="item-detail">
       <img src={item.image} alt={item.title} />
       <h2>{item.title}</h2>
       <p>{item.description}</p>
       <p>Preço: R${item.price}</p>
+      <ItemCount count={count} initialQuantity={1} onAdd={handleAddToCart} />
+      <button onClick={() => (window.location.href = "/cart")}>
+        Finalizar Compra
+      </button>
     </div>
   );
 }
